bb65c571a361d96c56f2d50395c55a616eb9bed7
lbicbiy <lbicbiy@44b37dae-b762-45cd-8c5e-82695f3631dd>
2010-10-22 12:49:00 +0000

    Косяк перевода в модуле сбербанка http://forum.myopencart.ru/viewtopic.php?f=12&t=569&view=unread#p6282
    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@4 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/catalog/language/russian/payment/fl_sberbank.php
+++ trunk/catalog/language/russian/payment/fl_sberbank.php
@@ -1,9 +1,9 @@
 <?php
 // Text
-$_['text_title']       = 'Квитанция Сбербанка РФ';
-$_['text_instruction'] = 'Для печати квитанции нажмите на кнопку &laquo;Печать Квитанции&raquo; (откроется в новом окне).';
-$_['text_instruction_2'] = '<a href="{server}index.php?route=payment/fl_sberbank/printpay&order_id=';
-$_['text_instruction_3'] = '" class="button" style="text-decoration:none;"><span>Печать Квитанции</span></a>';
-$_['text_payment']     = 'Заказ не будет обработан, пока деньги не поступят на наш рассчётный счёт.';
-$_['text_printpay']     = 'Печать Квитанции';
+$_['text_title']       		= 'Квитанция Сбербанка РФ';
+$_['text_instruction'] 		= 'Для печати квитанции нажмите на кнопку &laquo;Печать Квитанции&raquo; (откроется в новом окне).';
+$_['text_instruction_2'] 	= '<a href="{server}index.php?route=payment/fl_sberbank/printpay&order_id=';
+$_['text_instruction_3'] 	= '" class="button" style="text-decoration:none;"><span>Печать Квитанции</span></a>';
+$_['text_payment']     		= 'Заказ не будет обработан, пока деньги не поступят на наш расчётный счёт.';
+$_['text_printpay']     	= 'Печать Квитанции';
 ?>
