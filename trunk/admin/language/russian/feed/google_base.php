1bb57193c8fce00084abaf366dc62d276ebd83e7
ibabushka <ibabushka@44b37dae-b762-45cd-8c5e-82695f3631dd>
2010-12-31 13:01:29 +0000

    на перевод
    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@27 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/admin/language/russian/feed/google_base.php
+++ trunk/admin/language/russian/feed/google_base.php
@@ -8,7 +8,8 @@ $_['text_success']     = 'Настройки модуля обновлены!';
 
 // Entry
 $_['entry_status']     = 'Статус:';
-$_['entry_data_feed']  = 'Адрес:';
+$_['entry_data_feed']  = 'Адрес:<br/><span class="help">This url points to your feed. Paste it into your feed server.</span>';
+$_['entry_currency']   = 'Default Currency:<br/><span class="help">Google Base only supports USD, EUR, and GBP. If your store uses additional currencies, this is the currency the feed will use by default.</span>';
 
 // Error
 $_['error_permission'] = 'У Вас нет прав для управления этим модулем!';
