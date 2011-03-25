7d3c7e0175bdb4c7d795708acce3ab09c755fb88
Sammy95 <Sammy95@44b37dae-b762-45cd-8c5e-82695f3631dd>
2011-03-25 05:56:48 +0000

    FIX: исправлен синтаксис DROP TABLE при установке (драйвер MySQL)
    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@77 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/install/model/install.php
+++ trunk/install/model/install.php
@@ -138,7 +138,7 @@ class ModelInstall extends Model {
 		    }
 
 		    foreach ($data as $table) {
-			mysql_query('DROP table ' . $table , $connection);
+			mysql_query('DROP table `' . $table . '`', $connection);
 		    }
 		}
 		elseif ($db_driver == 'postgre')
