a3d60ab97ced40ec406e15f416a4122b4d981876
lbicbiy <lbicbiy@44b37dae-b762-45cd-8c5e-82695f3631dd>
2011-04-10 12:24:18 +0000

    http://opencartforum.ru/topic/1411-улучшим-перевод-вместе/page__view__findpost__p__11868
    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@103 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/catalog/language/russian/checkout/cart.php
+++ trunk/catalog/language/russian/checkout/cart.php
@@ -9,11 +9,11 @@ $_['text_weight']  	  = 'Вес корзины:';
 $_['text_error']      = 'Ваша корзина пуста!';
 
 // Column
-$_['column_remove']   = 'Удалить';
-$_['column_image']    = 'Изображение';
+$_['column_remove']   = '<span title="Убрать товар из корзины">Убр.</span>';
+$_['column_image']    = 'Фото';
 $_['column_name']     = 'Наименование';
 $_['column_model']    = 'Модель';
-$_['column_quantity'] = 'Количество';
+$_['column_quantity'] = '<span title="Количество">Кол-во</span>';
 $_['column_price']    = 'Цена';
 $_['column_total']    = 'Итого';
 
