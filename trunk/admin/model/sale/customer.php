1ab97a2332a2f8b210d5ec889b4d6459193d968f
uncleandy <uncleandy@44b37dae-b762-45cd-8c5e-82695f3631dd>
2011-04-20 13:26:27 +0000

    FIX: Админка: Не работала сортировка в продажах
    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@114 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/admin/model/sale/customer.php
+++ trunk/admin/model/sale/customer.php
@@ -154,7 +154,7 @@ class ModelSaleCustomer extends Model {
 		};
 
 		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
-			$sql .= " ORDER BY `" . $data['sort'] . '`';
+			$sql .= " ORDER BY " . $data['sort'];
 		} else {
 			$sql .= " ORDER BY `pname`";
 		}
