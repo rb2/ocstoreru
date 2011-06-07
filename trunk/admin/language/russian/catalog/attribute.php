dfceb8a484f9d59ab84f9555297190292a56279f
Nemo3000 <Nemo3000@44b37dae-b762-45cd-8c5e-82695f3631dd>
2011-06-07 17:32:56 +0000

    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@146 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/admin/language/russian/catalog/attribute.php
+++ trunk/admin/language/russian/catalog/attribute.php
@@ -1,23 +1,23 @@
 <?php
 // Heading
-$_['heading_title']          = 'Attributes';
+$_['heading_title']          = 'Атрибуты';
 
 // Text
-$_['text_success']           = 'Success: You have modified attributes!';
+$_['text_success']           = 'Атрибуты успешно обновлены!';
 
 // Column
-$_['column_name']            = 'Attribute Name';
-$_['column_attribute_group'] = 'Attribute Group';
-$_['column_sort_order']      = 'Sort Order';
-$_['column_action']          = 'Action';
+$_['column_name']            = 'Название атрибута';
+$_['column_attribute_group'] = 'Группа атрибута';
+$_['column_sort_order']      = 'Порядок сортировки';
+$_['column_action']          = 'Действие';
 
 // Entry
-$_['entry_name']            = 'Attribute Name:';
-$_['entry_attribute_group'] = 'Attribute Group:';
-$_['entry_sort_order']      = 'Sort Order:';
+$_['entry_name']            = 'Название атрибута:';
+$_['entry_attribute_group'] = 'Группа атрибута:';
+$_['entry_sort_order']      = 'Порядок сортировки:';
 
 // Error
-$_['error_permission']      = 'Warning: You do not have permission to modify attributes!';
-$_['error_name']            = 'Attribute Name must be between 3 and 64 characters!';
-$_['error_product']         = 'Warning: This attribute cannot be deleted as it is currently assigned to %s products!';
+$_['error_permission']      = 'У Вас нет прав для изменения атрибутов!';
+$_['error_name']            = 'Название атрибута должно быть от 3 до 64 символов!';
+$_['error_product']         = 'Этот атрибут не может быть удален, так как назначен %s товарам!';
 ?>
