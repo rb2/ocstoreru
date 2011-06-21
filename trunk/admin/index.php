42a5ec703757e2e6c9727e318b53d00374030a5b
ibabushka <ibabushka@44b37dae-b762-45cd-8c5e-82695f3631dd>
2011-06-21 12:40:19 +0000

    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@194 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/admin/index.php
+++ trunk/admin/index.php
@@ -1,12 +1,11 @@
 <?php
 // Version
-define('VERSION', '0.2.0');
+define('VERSION', '1.0.0');
 
 // Configuration
 require_once('config.php');
-@include_once(DIR_CONFIG  . 'config_tuning.php');
 
-// Install
+// Install 
 if (!defined('DIR_APPLICATION')) {
 	header('Location: ../install/index.php');
 	exit;
@@ -35,22 +34,25 @@ $registry->set('config', $config);
 // Database
 $db = new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 $registry->set('db', $db);
-
+		
 // Settings
-$query = $db->query("SELECT * FROM " . DB_PREFIX . "setting");
-
+$query = $db->query("SELECT * FROM " . DB_PREFIX . "setting WHERE store_id = '0'");
+ 
 foreach ($query->rows as $setting) {
 	$config->set($setting['key'], $setting['value']);
 }
 
-// Log
+// Url
+$url = new Url(HTTP_SERVER, HTTPS_SERVER);	
+$registry->set('url', $url);
+		
+// Log 
 $log = new Log($config->get('config_error_filename'));
 $registry->set('log', $log);
 
-// Error Handler
 function error_handler($errno, $errstr, $errfile, $errline) {
-	global $config, $log;
-
+	global $log, $config;
+	
 	switch ($errno) {
 		case E_NOTICE:
 		case E_USER_NOTICE:
@@ -68,21 +70,21 @@ function error_handler($errno, $errstr, $errfile, $errline) {
 			$error = 'Unknown';
 			break;
 	}
-
+		
 	if ($config->get('config_error_display')) {
 		echo '<b>' . $error . '</b>: ' . $errstr . ' in <b>' . $errfile . '</b> on line <b>' . $errline . '</b>';
 	}
-
+	
 	if ($config->get('config_error_log')) {
 		$log->write('PHP ' . $error . ':  ' . $errstr . ' in ' . $errfile . ' on line ' . $errline);
 	}
 
-	return TRUE;
+	return true;
 }
 
 // Error Handler
 set_error_handler('error_handler');
-
+		
 // Request
 $request = new Request();
 $registry->set('request', $request);
@@ -90,21 +92,20 @@ $registry->set('request', $request);
 // Response
 $response = new Response();
 $response->addHeader('Content-Type: text/html; charset=utf-8');
-$registry->set('response', $response);
-
-// Session
-$registry->set('session', new Session());
+$registry->set('response', $response); 
 
 // Cache
-$registry->set('cache', new Cache());
+$cache = new Cache();
+$registry->set('cache', $cache); 
 
-// Document
-$registry->set('document', new Document());
+// Session
+$session = new Session();
+$registry->set('session', $session); 
 
 // Language
 $languages = array();
 
-$query = $db->query("SELECT * FROM " . DB_PREFIX . "language");
+$query = $db->query("SELECT * FROM " . DB_PREFIX . "language"); 
 
 foreach ($query->rows as $result) {
 	$languages[$result['code']] = $result;
@@ -112,13 +113,18 @@ foreach ($query->rows as $result) {
 
 $config->set('config_language_id', $languages[$config->get('config_admin_language')]['language_id']);
 
+// Language	
 $language = new Language($languages[$config->get('config_admin_language')]['directory']);
-$language->load($languages[$config->get('config_admin_language')]['filename']);
-$registry->set('language', $language);
+$language->load($languages[$config->get('config_admin_language')]['filename']);	
+$registry->set('language', $language); 		
 
+// Document
+$document = new Document();
+$registry->set('document', $document); 		
+		
 // Currency
-$registry->set('currency', new Currency($registry));
-
+$registry->set('currency', new Currency($registry));		
+		
 // Weight
 $registry->set('weight', new Weight($registry));
 
@@ -127,7 +133,7 @@ $registry->set('length', new Length($registry));
 
 // User
 $registry->set('user', new User($registry));
-
+						
 // Front Controller
 $controller = new Front($registry);
 
---
+++ trunk/admin/language/english/catalog/manufacturer.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']      = 'Manufacturer';
+
+// Text
+$_['text_success']       = 'Success: You have modified manufacturers!';
+$_['text_default']       = 'Default';
+$_['text_image_manager'] = 'Image Manager';
+$_['text_percent']       = 'Percentage';
+$_['text_amount']        = 'Fixed Amount';
+
+// Column
+$_['column_name']        = 'Manufacturer Name';
+$_['column_sort_order']  = 'Sort Order';
+$_['column_action']      = 'Action';
+
+// Entry
+$_['entry_name']         = 'Manufacturer Name:';
+$_['entry_store']        = 'Stores:';
+$_['entry_keyword']      = 'SEO Keyword:';
+$_['entry_image']        = 'Image:';
+$_['entry_sort_order']   = 'Sort Order:';
+$_['entry_type']         = 'Type:';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify manufacturers!';
+$_['error_name']         = 'Manufacturer Name must be between 3 and 64 characters!';
+$_['error_product']      = 'Warning: This manufacturer cannot be deleted as it is currently assigned to %s products!';
+?>
