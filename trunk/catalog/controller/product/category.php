58a4f8a1ebef23abdf4e9d7d377e39bcc0743d3a
rb <rb@44b37dae-b762-45cd-8c5e-82695f3631dd>
2011-08-04 20:45:39 +0000

    substr -> mb_substr (долой "чёрные ромбики" в обрезанных описаниях товаров)
    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@268 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/catalog/controller/product/category.php
+++ trunk/catalog/controller/product/category.php
@@ -179,11 +179,17 @@ class ControllerProductCategory extends Controller {
 					$rating = false;
 				}
 								
+				$cut_descr_symbols = 400;
+				$descr_plaintext = strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'));
+				if( mb_strlen($descr_plaintext, 'UTF-8') > $cut_descr_symbols )
+				{
+					$descr_plaintext = mb_substr($descr_plaintext, 0, $cut_descr_symbols, 'UTF-8') . '&nbsp;&hellip;';
+				}
 				$this->data['products'][] = array(
 					'product_id'  => $result['product_id'],
 					'thumb'       => $image,
 					'name'        => $result['name'],
-					'description' => substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, 100) . '..',
+					'description' => $descr_plaintext,
 					'price'       => $price,
 					'special'     => $special,
 					'tax'         => $tax,
--- trunk/catalog/controller/product/compare.php
+++ trunk/catalog/controller/product/compare.php
@@ -113,7 +113,7 @@ class ControllerProductCompare extends Controller {
 					'thumb'        => $image,
 					'price'        => $price,
 					'special'      => $special,
-					'description'  => substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, 200) . '..',
+					'description'  => mb_substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, 200, 'UTF-8') . '..',
 					'model'        => $product_info['model'],
 					'manufacturer' => $product_info['manufacturer'],
 					'availability' => $availability,
