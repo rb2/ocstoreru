c0e0238683a4e51e006473bedd671248dc02accf
rb <rb@44b37dae-b762-45cd-8c5e-82695f3631dd>
2011-08-31 17:03:56 +0000

    re: r260 [260] admin: не показываелись скрытые категории в древовидно-ступенчатом списке категорий (см. changeset:260)
    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@273 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/admin/model/catalog/category.php
+++ trunk/admin/model/catalog/category.php
@@ -202,7 +202,9 @@ class ModelCatalogCategory extends Model {
 		$category_data = $this->cache->get('category.all.' . $this->config->get('config_language_id') . '.' . (int)$this->config->get('config_store_id'));
 
 		if (!$category_data || !is_array($category_data)) {
-			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE cd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "'  AND c.status = '1' ORDER BY c.parent_id, c.sort_order, cd.name");
+			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE cd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id')
+				// . "'  AND c.status = '1' ORDER BY c.parent_id, c.sort_order, cd.name");
+				. "'  ORDER BY c.parent_id, c.sort_order, cd.name");
 
 			$category_data = array();
 			foreach ($query->rows as $row) {
