bf48fea6cb1fbc7fbb05cd06a2b35345d7189262
AlexeySVN <AlexeySVN@44b37dae-b762-45cd-8c5e-82695f3631dd>
2011-10-18 14:27:52 +0000

    add rel="nofollow"
    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@293 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/catalog/language/russian/common/header.php
+++ trunk/catalog/language/russian/common/header.php
@@ -5,7 +5,7 @@ $_['text_wishlist']  = 'Заметки (%s)';
 $_['text_cart']      = 'Корзина';
 $_['text_items']     = '%s шт. - %s';
 $_['text_search']    = 'Поиск';
-$_['text_welcome']   = 'Войти в <a href="%s">личный кабинет</a> или <a href="%s">зарегистрироваться</a>.';
+$_['text_welcome']   = 'Войти в <a rel="nofollow" href="%s">личный кабинет</a> или <a rel="nofollow" href="%s">зарегистрироваться</a>.';
 $_['text_logged']    = 'Вы вошли как <a href="%s">%s</a> <b>(</b> <a href="%s">Выйти</a> <b>)</b>';
 $_['text_account']   = 'Личный Кабинет';
 $_['text_checkout']  = 'Оформить';
