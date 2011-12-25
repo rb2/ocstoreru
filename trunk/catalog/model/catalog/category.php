d7b4695873a271af56f79601952b9ed22a047c1e
yesvik <yesvik@44b37dae-b762-45cd-8c5e-82695f3631dd>
2011-12-25 20:16:40 +0000

    Альтернативная модель категорий
    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@320 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/catalog/model/catalog/category.php
+++ trunk/catalog/model/catalog/category.php
@@ -1,49 +1,56 @@
 <?php
 class ModelCatalogCategory extends Model {
 	public function getCategory($category_id) {
-		$query = $this->db->query("SELECT DISTINCT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.category_id = '" . (int)$category_id . "' AND cd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "' AND c.status = '1'");
-		
-		return $query->row;
+		return $this->getCategories((int)$category_id, 'by_id');
 	}
-	
-	public function getCategories($parent_id = 0) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.parent_id = '" . (int)$parent_id . "' AND cd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "'  AND c.status = '1' ORDER BY c.sort_order, LCASE(cd.name)");
-		
-		return $query->rows;
+
+	public function getCategories($id = 0, $type = 'by_parent') {
+		static $data = null;
+
+		if ($data === null) {
+			$data = array();
+
+			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE cd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "' AND c.status = '1' ORDER BY c.parent_id, c.sort_order, cd.name");
+
+			foreach ($query->rows as $row) {
+				$data['by_id'][$row['category_id']] = $row;
+				$data['by_parent'][$row['parent_id']][] = $row;
+			}
+		}
+
+		return ((isset($data[$type]) && isset($data[$type][$id])) ? $data[$type][$id] : array());
 	}
 
 	public function getCategoriesByParentId($category_id) {
 		$category_data = array();
-		
-		$category_query = $this->db->query("SELECT category_id FROM " . DB_PREFIX . "category WHERE parent_id = '" . (int)$category_id . "'");
-		
-		foreach ($category_query->rows as $category) {
+
+		$categories = $this->getCategories((int)$category_id);
+
+		foreach ($categories as $category) {
 			$category_data[] = $category['category_id'];
-			
+
 			$children = $this->getCategoriesByParentId($category['category_id']);
-			
+
 			if ($children) {
 				$category_data = array_merge($children, $category_data);
-			}			
+			}
 		}
-		
+
 		return $category_data;
 	}
-		
+
 	public function getCategoryLayoutId($category_id) {
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category_to_layout WHERE category_id = '" . (int)$category_id . "' AND store_id = '" . (int)$this->config->get('config_store_id') . "'");
-		
+
 		if ($query->num_rows) {
 			return $query->row['layout_id'];
 		} else {
 			return $this->config->get('config_layout_category');
 		}
 	}
-					
+
 	public function getTotalCategoriesByCategoryId($parent_id = 0) {
-		$query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.parent_id = '" . (int)$parent_id . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "' AND c.status = '1'");
-		
-		return $query->row['total'];
+		return count($this->getCategories((int)$parent_id));
 	}
 }
 ?>
