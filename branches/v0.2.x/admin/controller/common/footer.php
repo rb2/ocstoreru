e7c8e0dfcfb86281c2c2584bb32dc8dcd395a548
Sammy95 <Sammy95>
2012-02-10 19:30:05 +0000

    SECURITY: сокрытие версии движка на странице авторизации администратора
    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@352 44b37dae-b762-45cd-8c5e-82695f3631dd

--- branches/v0.2.x/admin/controller/common/footer.php
+++ branches/v0.2.x/admin/controller/common/footer.php
@@ -3,7 +3,8 @@ class ControllerCommonFooter extends Controller {
 	protected function index() {
 		$this->load->language('common/footer');
 		
-		$this->data['text_footer'] = sprintf($this->language->get('text_footer'), VERSION);
+		$version = $this->user->isLogged() ? VERSION : 'for WEB';
+		$this->data['text_footer'] = sprintf($this->language->get('text_footer'), $version);
 		
 		$this->id       = 'footer';
 		$this->template = 'common/footer.tpl';
