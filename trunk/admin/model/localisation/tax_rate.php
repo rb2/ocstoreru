d312f5445f62c42c895affdbb8fac0fc9382f712
yesvik <yesvik>
2012-05-08 10:21:08 +0000

    OC rev. 1001 Add new tax rate: Last update date shown incorrect. fix
    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@386 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/admin/model/localisation/tax_rate.php
+++ trunk/admin/model/localisation/tax_rate.php
@@ -1,7 +1,7 @@
 <?php 
 class ModelLocalisationTaxRate extends Model {
 	public function addTaxRate($data) {
-		$this->db->query("INSERT INTO " . DB_PREFIX . "tax_rate SET name = '" . $this->db->escape($data['name']) . "', rate = '" . (float)$data['rate'] . "', `type` = '" . $this->db->escape($data['type']) . "', geo_zone_id = '" . (int)$data['geo_zone_id'] . "', date_added = NOW()");
+		$this->db->query("INSERT INTO " . DB_PREFIX . "tax_rate SET name = '" . $this->db->escape($data['name']) . "', rate = '" . (float)$data['rate'] . "', `type` = '" . $this->db->escape($data['type']) . "', geo_zone_id = '" . (int)$data['geo_zone_id'] . "', date_added = NOW(), date_modified = NOW()");
 		
 		$tax_rate_id = $this->db->getLastId();
 		
