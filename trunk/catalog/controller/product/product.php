22b6564c7d4b6fdd9f333b060d1a1f6675c72135
yesvik <yesvik>
2012-05-08 13:48:59 +0000

    OC rev. 1012 stock options on the product page when the 'Stock Checkout' in the store settings
    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@398 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/catalog/controller/product/product.php
+++ trunk/catalog/controller/product/product.php
@@ -258,7 +258,7 @@ class ControllerProductProduct extends Controller {
 					$option_value_data = array();
 					
 					foreach ($option['option_value'] as $option_value) {
-						if (!$option_value['subtract'] || ($option_value['quantity'] > 0)) {
+						if (!$option_value['subtract'] || ($option_value['quantity'] > 0) || $this->config->get('config_stock_checkout')) {
 							if ((($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) && (float)$option_value['price']) {
 								$price = $this->currency->format($this->tax->calculate($option_value['price'], $product_info['tax_class_id'], $this->config->get('config_tax')));
 							} else {
