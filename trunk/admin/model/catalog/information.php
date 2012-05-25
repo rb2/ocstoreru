ed53cbe44815a4b22ec34ec5176475ac87bbaea6
yesvik <yesvik>
2012-05-25 19:40:10 +0000

    Слияние с ревизией(ями) 1110-1111 из OC:
    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@497 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/admin/model/catalog/information.php
+++ trunk/admin/model/catalog/information.php
@@ -1,14 +1,10 @@
 <?php
 class ModelCatalogInformation extends Model {
 	public function addInformation($data) {
-		$this->db->query("INSERT INTO " . DB_PREFIX . "information SET sort_order = '" . (int)$data['sort_order'] . "', status = '" . (int)$data['status'] . "'");
+		$this->db->query("INSERT INTO " . DB_PREFIX . "information SET sort_order = '" . (int)$data['sort_order'] . "', bottom = '" . (isset($data['bottom']) ? (int)$data['bottom'] : 0) . "', status = '" . (int)$data['status'] . "'");
 
 		$information_id = $this->db->getLastId(); 
 		
-		if (isset($data['bottom'])) {
-			$this->db->query("UPDATE " . DB_PREFIX . "information SET bottom = '" . (int)$data['bottom'] . "' WHERE information_id = '" . (int)$information_id . "'");
-		}
-			
 		foreach ($data['information_description'] as $language_id => $value) {
 			$this->db->query("INSERT INTO " . DB_PREFIX . "information_description SET information_id = '" . (int)$information_id . "', language_id = '" . (int)$language_id . "', title = '" . $this->db->escape($value['title']) . "', description = '" . $this->db->escape($value['description']) . "', meta_keyword = '" . $this->db->escape($value['meta_keyword']) . "', meta_description = '" . $this->db->escape($value['meta_description']) . "', seo_title = '" . $this->db->escape($value['seo_title']) . "', seo_h1 = '" . $this->db->escape($value['seo_h1']) . "'");
 		}
@@ -35,13 +31,7 @@ class ModelCatalogInformation extends Model {
 	}
 	
 	public function editInformation($information_id, $data) {
-		$this->db->query("UPDATE " . DB_PREFIX . "information SET sort_order = '" . (int)$data['sort_order'] . "', status = '" . (int)$data['status'] . "' WHERE information_id = '" . (int)$information_id . "'");
-
-		if (isset($data['bottom'])) {
-			$this->db->query("UPDATE " . DB_PREFIX . "information SET bottom = '1' WHERE information_id = '" . (int)$information_id . "'");
-		} else {
-			$this->db->query("UPDATE " . DB_PREFIX . "information SET bottom = '0' WHERE information_id = '" . (int)$information_id . "'");
-		}
+		$this->db->query("UPDATE " . DB_PREFIX . "information SET sort_order = '" . (int)$data['sort_order'] . "', bottom = '" . (isset($data['bottom']) ? (int)$data['bottom'] : 0) . "', status = '" . (int)$data['status'] . "' WHERE information_id = '" . (int)$information_id . "'");
 		
 		$this->db->query("DELETE FROM " . DB_PREFIX . "information_description WHERE information_id = '" . (int)$information_id . "'");
 					
