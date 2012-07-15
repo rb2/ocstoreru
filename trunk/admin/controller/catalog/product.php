28a1ff0d77991a270de407155c70957f821a951f
yesvik <yesvik>
2012-07-15 12:19:26 +0000

    Слияние с ревизией(ями) 1154 из OC:
    git-svn-id: http://subversion.assembla.com/svn/ocstoreru@542 44b37dae-b762-45cd-8c5e-82695f3631dd

--- trunk/admin/controller/catalog/product.php
+++ trunk/admin/controller/catalog/product.php
@@ -1298,7 +1298,7 @@ class ControllerCatalogProduct extends Controller {
 				
 				$json[] = array(
 					'product_id' => $result['product_id'],
-					'name'       => html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8'),	
+					'name'       => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8')),	
 					'model'      => $result['model'],
 					'option'     => $option_data,
 					'price'      => $result['price']
